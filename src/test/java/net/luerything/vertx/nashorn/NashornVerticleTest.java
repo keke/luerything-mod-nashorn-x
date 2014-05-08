d98b7d6b6bc9a9a685bde2ccc613ee95b47c0e8c
keke <iamkeke@gmail.com>
2014-05-08 21:08:39 +0800

    make it own repository

--- src/test/java/net/luerything/vertx/nashorn/NashornVerticleTest.java
+++ src/test/java/net/luerything/vertx/nashorn/NashornVerticleTest.java
@@ -4,6 +4,7 @@ package net.luerything.vertx.nashorn;
 import org.junit.Assert;
 import org.junit.Test;
 import org.vertx.java.core.Vertx;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Container;
 
 import static org.mockito.Matchers.eq;
@@ -12,11 +13,14 @@ import static org.mockito.Mockito.*;
 public class NashornVerticleTest
 {
   private NashornVerticleFactory factory = new NashornVerticleFactory();
+  private Container mockContainer = mock(Container.class);
 
   @Test
   public void testStart() throws Exception
   {
-    factory.init(mock(Vertx.class), mock(Container.class), getClass().getClassLoader());
+    JsonObject config = new JsonObject();
+    when(mockContainer.config()).thenReturn(config);
+    factory.init(mock(Vertx.class), mockContainer, getClass().getClassLoader());
     NashornVerticle verticle = (NashornVerticle) factory.createVerticle("abc");
     verticle.start();
     assert verticle.getContext().getAttribute(NashornVerticle.JVERTX) != null;
@@ -30,8 +34,9 @@ public class NashornVerticleTest
     ClassLoader mockLoader = mock(ClassLoader.class);
     when(mockLoader.getResource(eq("js/__boot__.js"))).thenReturn(getClass().getClassLoader().getResource
         ("js/__boot__.js"));
-
-    factory.init(mock(Vertx.class), mock(Container.class), mockLoader);
+    JsonObject config = new JsonObject();
+    when(mockContainer.config()).thenReturn(config);
+    factory.init(mock(Vertx.class), mockContainer, mockLoader);
     NashornVerticle verticle = (NashornVerticle) factory.createVerticle("abc");
     verticle.start();
 //    reset(mockLoader);
